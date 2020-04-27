35e3f7a4a5027504f53ae7a4ee3b4560c875a4cf
alltheatreides <31647732+alltheatreides@users.noreply.github.com>
2020-04-27 20:35:17 +0200

    hexaco_0.4
    - Refined on_startup trait event, everyone should get the new traits
    - Starting skeleton for the new childhood and education focus

---
+++ common/defines/AVE_MARIA_hexaco_defines.lua
@@ -0,0 +1,5 @@
+-- Character
+NDefines.NCharacter.MAX_WARDS_PER_GUARDIAN = 4
+NDefines.NCharacter.CHANGE_FOCUS_YEARS = 1
+NDefines.NCharacter.MAX_GENERATED_TRAITS_FOR_HISTORICAL = 10
+NDefines.NCharacter.BASE_NR_OF_CHILDREN_PER_COUPLE = 5
