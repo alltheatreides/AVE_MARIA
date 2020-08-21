117f279db427bc3dcf72fa6c626475751f1a38da
alltheatreides <31647732+alltheatreides@users.noreply.github.com>
2020-08-21 20:56:16 +0200

    Commanders Fix
    - Fixed relic of De Re Militari fiddling and testing

--- common/defines/AVE_MARIA_de_re_militari_defines.lua
+++ common/defines/AVE_MARIA_de_re_militari_defines.lua
@@ -1,4 +1,4 @@
 -- Military
-NDefines.NMilitary.MAX_COMMANDERS_DUKE = 3
-NDefines.NMilitary.MAX_COMMANDERS_KING = 3
-NDefines.NMilitary.MAX_COMMANDERS_EMPEROR = 3
+-- NDefines.NMilitary.MAX_COMMANDERS_DUKE = 3
+-- NDefines.NMilitary.MAX_COMMANDERS_KING = 3
+-- NDefines.NMilitary.MAX_COMMANDERS_EMPEROR = 3
